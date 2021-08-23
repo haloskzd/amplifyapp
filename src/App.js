12dae426b5a844da0ab4583ab6b060af78658d2d
Zach Haloski <zacharyhaloski@yahoo.com>
2021-08-22 20:59:11 -0400

    Trigger redeploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+
 import { API, Storage } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
